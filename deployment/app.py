e2890b8467f0a00bf41e51debaa2d67715f733a5
fhrzn <fahri.lafa@gmail.com>
2025-03-08 20:29:46 +0700

    update pickled models

--- deployment/app.py
+++ deployment/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from typing import Optional
+from fastapi.responses import JSONResponse
 from inference import Inference
 from pydantic import BaseModel
 
-# declare Student data structure class
+
+# define body payload schema
 class Student(BaseModel):
     gender: str
     race: str
@@ -14,59 +15,33 @@ class Student(BaseModel):
     read_score: float
     write_score: float
 
+
 app = FastAPI()
+
 
 @app.get("/")
 def home():
-    return {"message": "Hello World! - This message was sent from FastAPI"}
+    return JSONResponse(
+        content={"message": "Hello World! - This message was sent from FastAPI"}
+    )
 
-@app.post('/predict')
+
+@app.post("/predict")
 async def make_prediction(data: Student):
-    data = data.dict()
+    data = data.model_dump()
 
     data = {
-        'gender': data['gender'].lower(),
-        'race/ethnicity': data['race'],
-        'parental level of education': data['parental_edu'].lower(),
-        'lunch': data['lunch'].lower(),
-        'test preparation course': data['test_prep'],
-        'math score': data['math_score'],
-        'reading score': data['read_score'],
-        'writing score': data['write_score']
-    }    
+        "gender": data["gender"].lower(),
+        "race/ethnicity": data["race"],
+        "parental level of education": data["parental_edu"].lower(),
+        "lunch": data["lunch"].lower(),
+        "test preparation course": data["test_prep"],
+        "math score": data["math_score"],
+        "reading score": data["read_score"],
+        "writing score": data["write_score"],
+    }
 
     infer = Inference()
     predicted = infer.make_prediction(data)
 
-    return {
-        'predicted_score': predicted['predicted_score'][0]
-    }
-
-@app.get('/predict')
-async def make_prediction(
-    gender: str,
-    race: str,
-    parental_edu: str,
-    lunch: str,
-    test_prep: str,
-    math_score: float,
-    read_score: float,
-    write_score: float,
-    ):
-    data = {
-        'gender': gender.lower(),
-        'race/ethnicity': race,
-        'parental level of education': parental_edu.lower(),
-        'lunch': lunch.lower(),
-        'test preparation course': test_prep,
-        'math score': math_score,
-        'reading score': read_score,
-        'writing score': write_score
-    }    
-
-    infer = Inference()
-    predicted = infer.make_prediction(data)
-
-    return {
-        'predicted_score': predicted['predicted_score'][0]
-    }
+    return JSONResponse(content={"predicted_score": predicted["predicted_score"][0]})
